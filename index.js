ab9ddca4641df3dbe091b6ee7bb76c64e95c6cf0
Maria Negri <mariamnegri@gmail.com>
2021-11-06 15:52:42 -0300

    wip

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+require("dotenv").config()
+
+const { PORT } = require('./config')
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.use('*',(req,res)=>{
+    res.status(500).json({
+        message:"Incorrect path"
+    })
+})
+
+server.listen( PORT,()=>{
+    console.log(`Server listening in port ${PORT}`)
+})
